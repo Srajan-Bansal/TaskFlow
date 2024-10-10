bd5caef39dfaa1936899bace307dbb0d3514c3ac
Srajan Bansal <srajanbansal999@gmail.com>
2024-10-10 16:28:28 +0530

    latest:updated

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,42 +1,12 @@
-import { SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import { Button } from '@repo/ui';
-import {
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom';
+import './App.css';
 
-const App = () => {
-	const router = createBrowserRouter(
-		createRoutesFromElements(
-			<>
-				<Route
-					path='/'
-					element={
-						<div>
-							Home
-							<Button />
-						</div>
-					}
-				/>
-				<Route
-					path='sign-in'
-					element={<SignInButton />}
-				/>
-				<Route
-					path='sign-out'
-					element={<SignedOut />}
-				/>
-				<Route
-					path='user'
-					element={<UserButton />}
-				/>
-			</>
-		)
+function App() {
+	return (
+		<>
+			<Button></Button>
+		</>
 	);
-
-	return <RouterProvider router={router} />;
-};
+}
 
 export default App;
---
+++ packages/ui/src/index.ts
@@ -0,0 +1,6 @@
+import './index.css';
+
+// utils
+
+// components
+export { Button } from './components/Button';
