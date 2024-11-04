68c1746f9701bab906c84233cb8a1e84fa2e3b6b
Srajan Bansal <srajanbansal999@gmail.com>
2024-11-04 16:29:13 +0530

    refractor: web configuration

--- apps/web/src/config.ts
+++ apps/web/src/config.ts
@@ -1,4 +1,39 @@
+import { type Node, type Edge, DefaultEdgeOptions } from '@xyflow/react';
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL as string;
 const HOOKS_URL = import.meta.env.VITE_HOOKS_URL as string;
 
-export { BACKEND_URL, HOOKS_URL };
+const initialNodes: Node[] = [
+	{
+		id: '1',
+		type: 'trigger',
+		position: { x: 250, y: 50 },
+		data: { label: 'Add Trigger' },
+	},
+	{
+		id: '2',
+		type: 'action',
+		position: { x: 250, y: 200 },
+		data: { label: 'Add Action' },
+	},
+];
+
+const initialEdges: Edge[] = [
+	{ id: 'e1-2', source: '1', target: '2', animated: true },
+];
+
+const defaultEdgeOptions: DefaultEdgeOptions = {
+	type: 'simplebezier',
+	animated: true,
+	style: {
+		stroke: '#374151',
+	},
+};
+
+export {
+	BACKEND_URL,
+	HOOKS_URL,
+	initialNodes,
+	initialEdges,
+	defaultEdgeOptions,
+};
--- apps/web/src/pages/Create.tsx
+++ apps/web/src/pages/Create.tsx
@@ -10,11 +10,9 @@ import {
 	addEdge,
 	Handle,
 	Position,
-	Panel,
 	OnNodesChange,
 	OnEdgesChange,
 	OnConnect,
-	DefaultEdgeOptions,
 	type Node,
 	type Edge,
 	BackgroundVariant,
@@ -30,11 +28,10 @@ import {
 	availableTrigger,
 	NodeConnection,
 } from '../types/types';
-import { FlowModal } from '../components/FlowModel';
 import { SideModel } from '../components/SideModel';
-import { useAvailableActionsAndTriggers } from '../hooks/useAvailableActoinsAndTriggers';
 import { createTask, getTask, updateTask } from '../lib/api';
 import { showErrorToast } from '../lib/toaster';
+import { initialNodes, initialEdges, defaultEdgeOptions } from '../config';
 
 const triggerNode = ({ data }: { data: TriggerNode['data'] }) => (
 	<div className='bg-yellow-100 p-4 rounded-lg border border-yellow-300'>
@@ -70,33 +67,6 @@ const nodeTypes = {
 	trigger: triggerNode,
 	action: actionNode,
 } as const;
-
-const initialNodes: Node[] = [
-	{
-		id: '1',
-		type: 'trigger',
-		position: { x: 250, y: 50 },
-		data: { label: 'Add Trigger' },
-	},
-	{
-		id: '2',
-		type: 'action',
-		position: { x: 250, y: 200 },
-		data: { label: 'Add Action' },
-	},
-];
-
-const initialEdges: Edge[] = [
-	{ id: 'e1-2', source: '1', target: '2', animated: true },
-];
-
-const defaultEdgeOptions: DefaultEdgeOptions = {
-	type: 'simplebezier',
-	animated: true,
-	style: {
-		stroke: '#374151',
-	},
-};
 
 export const Create = () => {
 	const [nodes, setNodes] = useState<Node[]>(initialNodes);
